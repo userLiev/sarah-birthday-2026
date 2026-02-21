87cf6f2b9c8d53467dfddf0b59a10ffbfcad3cb8
Gabriel Coelho <gabriellc2307@gmail.com>
2026-02-21 18:13:59 -0300

    fix: things

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   // Em GitHub Actions, GITHUB_REPOSITORY = "owner/repo-name"
   // O base precisa ser "/repo-name/" para GitHub Pages funcionar corretamente.
   // Localmente (sem a variável), usa "/" para não quebrar o dev server.
-  base: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/',
-})
+  base: process.env.GITHUB_REPOSITORY ? `/sarah-birthday-2026/` : "/",
+});
